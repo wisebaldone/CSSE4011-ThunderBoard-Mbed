a34caed5d865eeed34971bfb28897861f8534d79
Liam Hoffman <liamhoffman@d-i89-158-135.student.eduroam.uq.edu.au>
2018-03-05 13:40:50 +1000

    no message

---
+++ Examples/Blinky + RGB/main.cpp
@@ -0,0 +1,25 @@
+#include "mbed.h"
+#include "RGBS.h"
+
+DigitalOut led1(LED4);
+
+DigitalIn btn(BUTTON1);
+// main() runs in its own thread in the OS
+int main() {
+	int count = 0;
+	init_leds(1,1,1,1);
+	setLEDS(0, 0, 7);
+    while (true) {
+    	if (count == 0) {
+    		setLEDS(2, 0, 0);
+    	} else if (count == 1) {
+    		setLEDS(0, 2, 0);
+    	} else if (count == 2) {
+    		setLEDS(0, 0, 2);
+    		count = 0;
+    	}
+    	count++; 
+	    led1 = !led1;
+	    wait(0.25);
+    }
+}
