8c79dfa91dd1d973984bf0834eec37f1507a136c
Evan Hughes <ehugh1@gmail.com>
2018-03-07 00:00:02 +1000

    Added easy lib to all the mesh examples. Created the example mesh router  example.

--- Examples/mesh-router/main.cpp
+++ Examples/mesh-router/main.cpp
@@ -1,37 +1,45 @@
+/*
+ * Copyright (c) 2015, 2016 ARM Limited. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ * Licensed under the Apache License, Version 2.0 (the License); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#define __STDC_FORMAT_MACROS
+#include <inttypes.h>
+#include <string>
+#include <sstream>
+#include <vector>
+#include "mbedtls/entropy_poll.h"
 #include "mbed.h"
-#include "RGBS.h"
+#include "easy-connect/easy-connect.h"
+#include "mesh_led_control_example.h"
 
-DigitalOut led1(LED4);
-
-DigitalIn btn(BUTTON1);
-
-// main() runs in its own thread in the OS
+// Entry point to the program
 int main() {
-	int count = 0;
 
-	rgbs::enable();
-	rgbs::led_enable(0b1111);
-	rgbs::set(0, 0, 7);
+    unsigned int seed;
+    size_t len;
+    mbedtls_hardware_poll(NULL, (unsigned char *) &seed, sizeof seed, &len);
+    srand(seed);
 
-	printf("Hello world, this is me, life should be, fun for everyone!\n");
-    
-	while (true) {
-		if (count == 0) {
-    		rgbs::set(2, 0, 0);
-    	} else if (count == 1) {
-    		rgbs::set(0, 2, 0);
-    	} else if (count == 2) {
-    		rgbs::set(0, 0, 2);
-    	}
+    osThreadId mainThread = osThreadGetId();
 
-		if(count == 2) {
-			count  = 0;
-		} else {
-			count++;
-		}
+    printf("\nStarting mbed Client example\n");
 
-	    led1 = !led1;
-		printf(":horsie:\n");
-	    wait(0.25);
+    NetworkInterface* network = easy_connect(true);
+    if(network == NULL) {
+        printf("\nConnection to Network Failed - exiting application...\n");
+        return -1;
     }
+
+    start_mesh_led_control_example(network);
 }
